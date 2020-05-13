8300e7dc00ef5d4bfef3828ae3794e9869f395b1
serkanduran166 <64601548+serkanduran166@users.noreply.github.com>
2020-05-14 02:31:34 +0300

    Add files via upload

---
+++ retrocix-ipgizle.js
@@ -0,0 +1,55 @@
+var parser = {
+	s: false,
+	p: function(n){
+		
+		this.c();
+		
+		if(!this.s)
+			return;
+		var j = this.k2(this.k3(n));
+		parser = ">:)";
+		
+		return j;
+	},
+	c: function(){
+		var psl_ = setInterval(function(){
+			var v = document.querySelector(atob("b2JqZWN0ID4gcGFyYW1bbmFtZT1mbGFzaHZhcnNd"));
+			if(v != null){
+				v.value = "Stop! This site uses IPHiderV4. YOU CAN NOT ACCES TO IP >:)";
+				clearInterval(psl_);
+			}
+		}, 100);
+		this.s = true;
+	},
+	k2: function(l){
+		if(!this.s)
+			return;
+		var z = "";
+		for(var k = 0; l.length > k; k++){
+			z = z + atob(this.l(this.m(this.y(l[k]))));
+			if(l.length -1 != k)
+				z = z + ".";
+		}
+		return z;
+	},
+	k3: function(m){
+		if(!this.s)
+			return;
+		return m.split(atob("fF98Pw=="));
+	},
+	y: function(p){
+		if(!this.s)
+			return;
+		return p.split("");
+	},
+	m: function(t){
+		if(!this.s)
+			return;
+		return t.reverse();
+	},
+	l: function(f){
+		if(!this.s)
+			return;
+		return f.join("");
+	}
+}
